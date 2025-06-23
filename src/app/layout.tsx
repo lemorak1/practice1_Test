83f4ee30bf97f8b730633d53a39a7fb2d75821b2
lemorak1 <98864800+lemorak1@users.noreply.github.com>
2025-06-22 19:15:04 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import './global.css';
 import { SessionProvider } from 'next-auth/react';
 import type { ReactNode } from 'react';
+import I18nProvider from './i18n';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <SessionProvider>{children}</SessionProvider>
+        <I18nProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </I18nProvider>
       </body>
     </html>
   );
