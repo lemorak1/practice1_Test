aa9534935cc1614140079b945df312541022cbad
lemorak1 <98864800+lemorak1@users.noreply.github.com>
2025-06-22 19:14:26 -0500

    Update i18n.tsx

---
+++ src/app/i18n.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { appWithTranslation } from 'next-i18next';
+import type { ReactNode } from 'react';
+import nextI18NextConfig from '../next-i18next.config.js';
+
+function I18nProvider({ children }: { children: ReactNode }) {
+  return <>{children}</>;
+}
+
+export default appWithTranslation(I18nProvider, nextI18NextConfig);
